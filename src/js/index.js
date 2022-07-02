52927c8d59551f0ccf2d4af33db3919eab113789
kowalsking <kovvalsking@gmail.com>
2022-07-03 01:10:53 +0300

    oh boi i'm fcking hate frontend)))

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
+import { Renderer, Camera, Transform } from '../../ogl-master'
+
 class App {
   constructor() {
-    console.log('im working!')
+    this.createRenderer()
+  }
+
+  createRenderer() {
+    this.renderer = new Renderer({
+      alpha: true
+    })
+
+    this.gl = this.renderer.gl
+    document.body.append(this.gl.canvas)
   }
 }
 
