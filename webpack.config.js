25681ea4b61cf2bf11720d01bcb435a4678c3ecd
kowalsking <kovvalsking@gmail.com>
2022-07-04 00:14:22 +0300

    add images on site

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Infinite Scroll Demo!',
+      template: 'index.html',
+      minify: false
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
       }
     ]
   }
