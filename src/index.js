7be71a776aadacca659f10fe7492062200e63289
kowalsking <kovvalsking@gmail.com>
2022-07-04 00:39:27 +0300

    Creating OGL 3D environment

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ class App {
   constructor() {
     this.createRenderer()
     this.createCamera()
-
     this.createScene()
+
+    this.onResize()
+
+    this.update()
+
+    this.addEventListeners()
   }
 
   createRenderer() {
@@ -26,6 +31,53 @@ class App {
 
   createScene() {
     this.scene = new Transform()
+  }
+
+  /**
+   * Wheel.
+   */
+
+  onWheel() { }
+
+  /**
+   * Resize.
+   */
+
+  onResize() {
+    this.screen = {
+      height: window.innerHeight,
+      width: window.innerWidth
+    }
+
+    this.renderer.setSize(this.screen.width, this.screen.height)
+
+    this.camera.perspective({
+      aspect: this.gl.canvas.width / this.gl.canvas.height
+    })
+
+    const fov = this.camera.fov * (Math.PI / 180)
+    const height = 2 * Math.tan(fov / 2) * this.camera.position.z
+    const width = height * this.camera.aspect
+
+    this.viewport = {
+      height,
+      width
+    }
+  }
+
+  update() {
+    this.renderer.render({
+      scene: this.scene,
+      camera: this.camera
+    })
+
+    window.requestAnimationFrame(this.update.bind(this))
+  }
+
+  addEventListeners() {
+    window.addEventListener('resize', this.onResize.bind(this))
+    window.addEventListener('mousewheel', this.onWheel.bind(this))
+    window.addEventListener('wheel', this.onWheel.bind(this))
   }
 }
 
